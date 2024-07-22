411b5fb5cbc482d1c11d9e733469a48b6c72f2dd
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-07-22 14:10:47 +0400

    Write repository yaradıldı

--- İnfrastructure/ProjeWebApi.Persistence/Repositories/WriteRepository.cs
+++ İnfrastructure/ProjeWebApi.Persistence/Repositories/WriteRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using ProjeWebApi.Application.Interface.Repositories;
+using ProjeWebApi.Domen.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,51 @@ using System.Threading.Tasks;
 
 namespace ProjeWebApi.Persistence.Repositories
 {
-    public class WriteRepository
+    public class WriteRepository<T> : IWriteRepository<T> where T : class, IEntitybase, new()
     {
+        private readonly DbContext dbContext;
+
+        public WriteRepository(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        private DbSet<T> Table { get => dbContext.Set<T>(); }
+
+
+
+
+        public async Task AddAsync(T entity)
+        {
+            await Table.AddAsync(entity);
+        }
+
+
+
+        public async Task AddRangAsync(IList<T> entities)
+        {
+            await Table.AddRangeAsync(entities);
+        }
+
+
+
+        public async Task HardDeleteAsync(T entity)
+        {
+           await Task.Run(() => Table.Remove(entity));
+
+            
+        }
+
+
+
+     
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+           await Task.Run(() => Table.Update(entity));
+
+            return entity;
+        }
     }
 }
